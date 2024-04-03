37a5d75d37328c53a5106b8db101dc86dddf3400
Борис Алейников <airplanemodesnetwork@gmail.com>
2024-04-03 15:43:53 +0300

    revert

--- theme.js
+++ theme.js
@@ -1,16 +1,20 @@
-const cssfile = localStorage.getItem('theme') === 'dark' ? '3024.min.css' : 'github.min.css';
-let linkelement = document.createElement('link');
-linkelement.rel = 'stylesheet';
-linkelement.href = cssfile;
-document.head.appendChild(linkelement);
+let linktag = document.createElement("link");
+linktag.rel = "stylesheet";
+linktag.href = localStorage.getItem("theme") === "dark" ? "github.min.css" 
+                                                        : "3024.min.css";
+document.head.appendChild(linktag);
 
-tailwind.config = { darkMode: 'class' }
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
-    document.documentElement.classList.add('dark');
+tailwind.config = { darkMode: "class" };
+
+if (localStorage.theme === "dark" || 
+    !("theme" in localStorage)    && 
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+    document.documentElement.classList.add("dark");
 else 
-    document.documentElement.classList.remove('dark');
+    document.documentElement.classList.remove("dark");
 
 const toggle = () => {
-    localStorage.theme === 'light' ? localStorage.theme = 'dark' : localStorage.theme = 'light';
+    localStorage.theme === "light" ? localStorage.theme = "dark" 
+                                   : localStorage.theme = "light";
     window.location.reload();
 }
